23d1bb29624e8b4f06b6880310f9dd91a496e90d
Frank Boës <fboes@3960.org>
2019-01-09 07:31:52 +0100

    :pill: Maximum possible values match Aerofly FS 2 settings

--- test/metar-to-aerofly.js
+++ test/metar-to-aerofly.js
@@ -6,7 +6,7 @@ const metarToAerofly = require('../lib/metar-to-aerofly');
 
 describe('metarToAerofly', function() {
 
-  it('should convert METAR object to Aerofly object', function() {
+  it('must convert METAR object to Aerofly object', function() {
     const aeroflyObject = metarToAerofly({
       icao: 'KPIE',
       observed: new Date('2019-01-29T12:30:18.825Z'),
@@ -33,7 +33,36 @@ describe('metarToAerofly', function() {
     assert.strictEqual(aeroflyObject.clouds.length, 1, 'Clouds');
   });
 
-  it('should convert METAR object to Aerofly object #2', function() {
+  it('must have proper wind', function() {
+    const aeroflyObject = metarToAerofly({
+      icao: 'KPIE',
+      observed: new Date('2019-01-29T12:30:18.825Z'),
+      wind: { degrees: 270, speed_kts: 40, gust_kts: 40 },
+      visibility: { meters: 20000 },
+      clouds: [
+        { base_feet_agl: 2900, minDensity: 5, maxDensity: 7 }
+      ],
+      temperature: {
+        celsius: 18
+      },
+      barometer: {
+        kpa: 101.0
+      }
+    }, {
+      maxWind: 40
+    });
+
+    assert.ok(aeroflyObject);
+    assert.strictEqual(aeroflyObject.time_day,      29);
+    assert.strictEqual(aeroflyObject.time_month,    1);
+    assert.strictEqual(aeroflyObject.time_hours,    12.5);
+    assert.strictEqual(aeroflyObject.wind_direction_in_degree,  270);
+    assert.strictEqual(aeroflyObject.wind_strength, 1, 'Testing sqrt formula for 40kts');
+    assert.strictEqual(aeroflyObject.visibility,    1);
+    assert.strictEqual(aeroflyObject.clouds.length, 1);
+  });
+
+  it('must have proper wind #2', function() {
     const aeroflyObject = metarToAerofly({
       icao: 'KPIE',
       observed: new Date('2019-01-29T12:30:18.825Z'),
@@ -48,19 +77,21 @@ describe('metarToAerofly', function() {
       barometer: {
         kpa: 101.0
       }
+    }, {
+      maxWind: 20
     });
 
-    // console.log(aeroflyObject);
     assert.ok(aeroflyObject);
     assert.strictEqual(aeroflyObject.time_day,      29);
     assert.strictEqual(aeroflyObject.time_month,    1);
     assert.strictEqual(aeroflyObject.time_hours,    12.5);
     assert.strictEqual(aeroflyObject.wind_direction_in_degree,  270);
+    assert.strictEqual(aeroflyObject.wind_strength, 1, 'Testing sqrt formula for 40kts');
     assert.strictEqual(aeroflyObject.visibility,    1);
     assert.strictEqual(aeroflyObject.clouds.length, 1);
   });
 
-  it('should convert METAR object to Aerofly object #3', function() {
+  it('must convert METAR object to Aerofly object #3', function() {
     const aeroflyObject = metarToAerofly({
       icao: 'KPIE',
       observed: new Date('2019-01-29T18:30:18.825Z'),

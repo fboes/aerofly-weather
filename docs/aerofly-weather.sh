12690432df8dbd5b5ab044bd0de5dd09eb330461
Frank Boës <fboes@3960.org>
2019-01-14 18:42:20 +0100

    :wrench: Quoting shell variables

--- docs/aerofly-weather.sh
+++ docs/aerofly-weather.sh
@@ -7,7 +7,7 @@ METAR_URL_END=
 MCF_LOCATION="~\Documents\Aerofly FS 2"
 
 # Change directory
-pushd $MCF_LOCATION
+pushd ${MCF_LOCATION}
 
 # Backup
 echo "Backup of main.mcf"
@@ -22,37 +22,37 @@ do
   case $OPT in
     "METAR input")
       read -p 'Enter METAR string: ' METAR
-      aerofly-metar "$METAR" --verbose
+      aerofly-metar "${METAR}" --verbose
       ;;
 
     "METAR URL")
-      if [ -z "$METAR_URL" ]; then
+      if [ -z "${METAR_URL}" ]; then
         echo "Please set METAR_URL in line 5 / 6"
         exit 1
       fi
       read -p 'Enter ICAO string: ' ICAO
-      aerofly-metar-url $METAR_URL$ICAO$METAR_URL_END --verbose
+      aerofly-metar-url ${METAR_URL}${ICAO}${METAR_URL_END} --verbose
       ;;
 
     "Check URL")
-      if [ -z "$METAR_URL" ]; then
+      if [ -z "${METAR_URL}" ]; then
         echo "Please set METAR_URL in line 5 / 6"
         exit 1
       fi
-      start $METAR_URL
+      start ${METAR_URL}
       ;;
 
     "CheckWX")
-      if [ -z "$CHECKWX_APIKEY" ]; then
+      if [ -z "${CHECKWX_APIKEY}" ]; then
         echo "Please set CHECKWX_APIKEY in line 4"
         exit 1
       fi
       read -p 'Enter ICAO string: ' ICAO
-      aerofly-checkwx $ICAO $CHECKWX_APIKEY --verbose
+      aerofly-checkwx ${ICAO} ${CHECKWX_APIKEY} --verbose
       ;;
 
     "Start Aerofly FS 2")
-      # This is pure guess work and is not tested on actual PC
+      # This is pure guess work and is not tested on actual PCs
       start steam://rungameid/434030
       break;
       ;;
